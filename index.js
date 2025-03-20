004c7dca8585b4981efa7bdfd98eee05443af3a9
Digbijaya Biswal <digbijayabiswal@MacBook-Air.local>
2025-03-20 15:21:01 +0530

    User rount

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.post('/signup', (req, res) => {
+    res.send('Hello World!');
+})
+
+app.get('/login', (req, res) => {
+    res.send('Hello World!');
+})
+
+app.put('/updateuser', (req, res) => {
+    res.send('Hello World!');
+})
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+});
